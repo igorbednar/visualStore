2a3b99fee087f50023266b274c3c54dc2579488e
Bednarchik <ib185033@ncr.com>
2017-03-26 14:37:26 +0300

    Project structure

---
+++ visualStore.js
@@ -0,0 +1,19 @@
+//Visual store api, depends on open layers
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['ol'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('ol'));
+    } else {
+        // Browser globals (root is window)
+        root.vs = factory(root.ol);
+    }
+}(this, function (ol) {
+    var visualStore = {};
+
+    return visualStore.vs;
+}));
